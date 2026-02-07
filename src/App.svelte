5d824961174f1d43328e6c9cad78cab56729c7a4
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2026-02-07 16:38:27 +0700

    derived & derived.by

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  let count = $state(0);
+  let doubled = $derived(count * 2);
+  let numbers = $state([1, 2, 3]);
+  let total = $derived.by(() => {
+    let total = 0;
+    for (const n of numbers) {
+      total += n;
+    }
+    return total;
+  })
+</script>
+
+<main>
+  <button onclick={() => count++}>
+    {doubled}
+  </button>
+  <button onclick={() => numbers.push(numbers.length + 1)}>
+    {numbers.join(' + ')} = {total}
+  </button>
+</main>
+
+<style>
+</style>
+
+<!-- In essence, $derived(expression) is equivalent to $derived.by(() => expression) -->
+
+<!-- additionals -->
+<!-- To exempt a piece of state from being treated as a dependency, use untrack -->
