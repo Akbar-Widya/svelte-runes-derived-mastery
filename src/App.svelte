2739f2e3d9157998d2a05b1c708c4a12b9e61d65
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2026-02-07 17:01:43 +0700

    Update propagation

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,9 @@
 <script>
-/**
- * Derived and reactivity
- * When you bind to or modify a selected object that points to an element within a deeply reactive array, 
- * the changes automatically sync back to the original array.
-*/
-let items = $state([]);
-
-let index = $state(0);
-let selected = $derived(items[index]);
-
-/**
- * Destructuring
- * If you use destructuring with a $derived declaration, 
- * the resulting variables will all be reactive.
-*/
-let { a, b, c } = $derived(stuff());
-
-// is roughly equivalent to:
-let _stuff = $derived(stuff());
-let a = $derived(_stuff.a);
-let b = $derived(_stuff.b);
-let c = $derived(_stuff.c);
+  let count = $state(0);
+  let large = $derived(count > 10);
 </script>
+
+<!-- click 10 times to change into true -->
+<button onclick={() => count++}>
+  {large}
+</button>
